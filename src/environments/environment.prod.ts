7d6f693c12cee2cf27fafbf73e80893930c3e5dd
Mohamed Alaa <malaa@selecteg.net>
2022-05-16 16:04:15 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  trendingUrl: 'https://api.themoviedb.org/3/trending/',
+  apiTrending:'week?api_key=2e203d1deb36ecf317e51549c519f9ad',
+  apiMovie:'api_key=2e203d1deb36ecf317e51549c519f9ad&language=en-US',
+  movieUrl :'https://api.themoviedb.org/3/movie/',
+  apiAuth: 'https://route-egypt-api.herokuapp.com', 
 };
